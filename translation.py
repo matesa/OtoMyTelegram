a7f0d8307af099c38dceff64794a2ad3958631bb
matesa <75279583+matesa@users.noreply.github.com>
2020-12-31 00:02:16 +0300

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,16 @@
+class Translation(object):
+    START_TEXT = """Merhaba!
+@OtoTelegramBot APP-ID APP-HASH Alıcıya Hoşgeldiniz... 
+Lütfen Telefon numaranızı +90 lı Şekilde Giriniz
+Bilgilerinizi Yeniden Gİrmek İçin /start Butonunu Basınız"""
+    AFTER_RECVD_CODE_TEXT = """Son Olarak!
+Telegramdan Gelen Kodu Giriniz!
+
+Bu kod sadece my.telegram.org'dan APP-ID'sini Almak amacıyla kullanılır!.
+
+Bilgilerinizi Yeniden Gİrmek İçin /start Butonunu Basınız"""
+    BEFORE_SUCC_LOGIN = "Alınan kod. Web sayfasına giriliyor..."
+    ERRED_PAGE = "Hata uygulama kimliği alınamadı. \n\n@poyraz2103"
+    CANCELLED_MESG = "Hoşçakal! Bot görüşmesini yeniden başlatmak için"
+    IN_VALID_CODE_PVDED = "Üzgünüm, ancak giriş geçerli bir Telegram Web Oturum Açma kodu gibi görünmüyor"
+    IN_VALID_PHNO_PVDED = "Üzgünüm, ancak giriş geçerli bir Telefon numarası gibi gözükmüyor"
